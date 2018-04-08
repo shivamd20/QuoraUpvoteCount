b6680b378e6c7ed6e48cff2717f0e8c0b328cbba
Shivam Kumar Dwivedi <shivamd20@gmail.com>
2018-04-08 23:37:44 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+var answers = document.getElementsByClassName('feed_item inline_expand_item')
+
+setInterval(()=>{answers[answers.length-1].scrollIntoView();
+
+fun();
+
+},1000);
+
+
+var fun = ()=>{
+
+
+var count = 0;
+
+var answers = document.getElementsByClassName('feed_item inline_expand_item')
+
+for(let i=0;i<answers.length-1;i++){
+
+var upvoteString = answers[i].getElementsByClassName('icon_action_bar-count')[0].innerText.replace(' ·','').replace('k','000');
+
+
+
+count+=Number.parseInt(upvoteString)
+
+
+
+
+}
+
+console.log("Upvote count of  "+ answers.length +" questions are " +count);
+
+}
